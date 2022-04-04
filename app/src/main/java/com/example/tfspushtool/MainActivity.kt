69b549bb86833840c222ca5b2357636a096e65fd
XiaotongZhao <275451183@qq.com>
2022-04-04 12:20:25 +0800

    create tfspushTool
    create tfspushTool

---
+++ app/src/main/java/com/example/tfspushtool/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.tfspushtool
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tfspushtool.databinding.ActivityMainBinding
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.pushDataToTfs.setOnClickListener{ pushDataToTFS() }
+    }
+
+    fun pushDataToTFS() {
+        var tfsAddress = binding.tfsAddress.text.toString();
+        var tokenInformation = binding.tokenInformation .text.toString();
+        var userName = binding.userName.text.toString();
+
+    }
+}
