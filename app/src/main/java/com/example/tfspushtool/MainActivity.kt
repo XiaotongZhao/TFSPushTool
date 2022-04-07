f2a593dbf41f43d6cf22f99fc8d10b4aabc09fe5
XiaotongZhao <275451183@qq.com>
2022-04-07 18:25:27 +0800

    commit change code

--- app/src/main/java/com/example/tfspushtool/MainActivity.kt
+++ app/src/main/java/com/example/tfspushtool/MainActivity.kt
@@ -3,6 +3,14 @@ package com.example.tfspushtool
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tfspushtool.databinding.ActivityMainBinding
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.tfspushtool.network.MarsApi
+import com.example.tfspushtool.overview.OverviewViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,5 +28,21 @@ class MainActivity : AppCompatActivity() {
         var tokenInformation = binding.tokenInformation .text.toString();
         var userName = binding.userName.text.toString();
         var message =binding.message.text.toString();
+
+        try {
+            val queue = Volley.newRequestQueue(this)
+            var test = ""
+            val url = "https://android-kotlin-fun-mars-server.appspot.com/"
+            val stringRequest = StringRequest(
+                Request.Method.GET, url,
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    test = response
+                    Log.i("http",response);
+                },
+                Response.ErrorListener {   })
+        } catch (e: Exception) {
+            var ex = e;
+        }
     }
 }
---
+++ app/src/main/java/com/example/tfspushtool/network/MarsPhoto.kt
@@ -0,0 +1,10 @@
+package com.example.tfspushtool.network
+
+import com.squareup.moshi.Json
+
+data class MarsPhoto(
+    val id: String,
+    // used to map img_src from the JSON to imgSrcUrl in our class
+    @Json(name = "img_src")
+    val imgSrcUrl: String
+)
